537ef6bea25fe2082815583f6c57b792b5ba6b0f
“feifeiwei” <18801498693@163.com>
2021-12-26 23:50:17 +0800

    add 026 cpp

---
+++ 026.删除有序数组中的重复项/run.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        if (nums.size()==0)
+            return 0;
+
+        int slow = 0;
+
+        for(size_t fast=1; fast!=nums.size(); ++ fast){
+
+            if (nums[fast]!=nums[slow]){
+                slow ++;
+                nums[slow] = nums[fast];
+            }
+                
+        }
+
+
+        return slow + 1;
+
+
+
+
+
+    }
+};
+
+
+
