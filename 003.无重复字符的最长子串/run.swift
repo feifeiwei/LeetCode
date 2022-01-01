8bf03e064b6892e17947460421a6dfd5ca289a6f
“feifeiwei” <18801498693@163.com>
2022-01-02 00:08:50 +0800

    upload swift

--- 003.无重复字符的最长子串/run.swift
+++ 003.无重复字符的最长子串/run.swift
@@ -1,2 +1,34 @@
 //: A UIKit based Playground for presenting user interface
   
+import Foundation
+
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+
+        var D:[Character:Int] = [:]
+        
+        var start = 0
+        var maxLen:Int = 0
+        let sArray = Array(s)
+        
+        for (i, cur_str) in sArray.enumerated(){ // faster:
+//        for i in 0..<s.count{  // slow!
+//            let index = s.index(s.startIndex, offsetBy: i) // slow
+//            let cur_str = s[index]
+
+            
+            if D.keys.contains(cur_str) && start <= D[cur_str]!{
+                start = D[cur_str]! + 1
+            }
+            else{
+                maxLen = max(maxLen, i-start+1)
+            }
+            
+            D[cur_str] = i
+            
+        }
+        return maxLen
+
+    }
+}
