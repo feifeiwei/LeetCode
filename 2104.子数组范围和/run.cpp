d5f355ab4b1d7d805ae109a8746fff4ebf620394
feifeiwei <18801498693@163.com>
2022-09-19 00:22:16 +0800

    modified 2104

--- 2104.子数组范围和/run.cpp
+++ 2104.子数组范围和/run.cpp
@@ -20,10 +20,8 @@ using vectorIter = vector<int>::iterator;
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        
-
         long long res = 0;
-        
+        //暴力法
         for(int i=0; i<nums.size(); ++i){
             int minVal = INT_MAX, maxVal = INT_MIN;
             for(int j=i; j<nums.size(); ++j){
@@ -37,15 +35,37 @@ public:
 
         return res;
     }
+    
+    long long subArrayRanges2(vector<int>& nums) {
+        long long res = 0;
+        
+        size_t count = nums.size();
+        for(int i=0; i<count; ++i){
+            
+            long l_min=1, r_min=1;
+            while(i-l_min>=0 && nums[i-l_min]<=nums[i]) l_min++;
+            while(i+r_min<count && nums[i+r_min]<nums[i]) r_min++;
+            res += (r_min*l_min) * nums[i];
+            
+            long l_max=1, r_max=1;
+            while(i-l_max>=0 && nums[i-l_max] > nums[i]) l_max++;
+            while(i+r_max<count && nums[i+r_max] >= nums[i]) r_max++;
+            res -= (l_max*r_max) * nums[i];
+             
+        }
+        return res;
+        
+    }
+    
 };
 
 
 int main()
 {
-    vector<int> v{4,-2,-3,4,1};
+    vector<int> v{1,3,3};
     
     Solution s;
-    cout << s.subArrayRanges(v) << endl;
+    cout << s.subArrayRanges2(v) << endl;
     
     cout<<"done!\n";
 
--- README.md
+++ README.md
@@ -32,3 +32,4 @@
 |258|[各位相加](https://leetcode.cn/problems/add-digits/)|[C++](258.各位相加/run.cpp) [Python] [Swift]|[Note](258.各位相加/README.md)|Easy|
 |443|[压缩字符串](https://leetcode.cn/problems/string-compression/)|[C++](443.压缩字符串/run.cpp) [Python] [Swift]|[Note](443.压缩字符串/README.md)|Medium|
 |917|[仅仅反转字母](https://leetcode.cn/problems/reverse-only-letters/)|[C++](917.仅仅反转字母/run.cpp) [Python] [Swift]|[Note](917.仅仅反转字母/README.md)|Easy|
+|2104|[子数组范围和](https://leetcode.cn/problems/sum-of-subarray-ranges/)|[C++](2104.子数组范围和/run.cpp) [Python] [Swift]|[Note](2104.子数组范围和/README.md)|Medium|
